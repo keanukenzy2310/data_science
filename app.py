492e9bd953f6965aa26f1fff397376ddc7dfba55
keanukenzy2310 <azharamadhan23@gmail.com>
2025-04-23 11:30:17 +0700

    deployed model

---
+++ app.py
@@ -0,0 +1,17 @@
+pip install joblib
+pip install streamlit
+
+import streamlit as st
+import joblib
+import numpy as np
+
+lin_reg_loaded = joblib.load('lin_reg_model.joblib')
+
+st.title("Prediksi Gaji Berdasarkan Lama Bekerja")
+
+years_experience = st.number_input("Masukan jumlah tahun bekerja:", min_value=0.0, step=0.1)
+
+if st.button("Prediksi Gaji"):
+	gaji = lin_reg_loaded.predict([[years_experience]])
+	st.write(f"Gaji seseorang setelah bekerja selama {years_experience} tahun adalah ${gaji[0]:,.2f}
+
